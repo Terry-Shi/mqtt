682169171807758706424816f699655665a27776
terryemailbox@gmail.com <terryemailbox@gmail.com>
2019-07-18 11:10:35 +0800

    语义控制 for kafka

--- src/main/java/kafka/KafkaConsumerSample.java
+++ src/main/java/kafka/KafkaConsumerSample.java
@@ -18,14 +18,14 @@ public class KafkaConsumerSample {
 	public static void main(String[] args) {
 		Properties props = new Properties();
 		props.put("bootstrap.servers", "localhost:9092");
-		props.put("group.id", "group"); // 需要服务器端先配置／创建？
+		props.put("group.id", "group"); // 需要服务器端先配置／创建？ 不需要
 		props.put("enable.auto.commit", "false"); // 关闭自动commit
 		props.put("session.timeout.ms", "30000");
-		props.put("auto.offset.reset", "earliest");
+		props.put("auto.offset.reset", "earliest"); // 初次连接上从哪里开始取数据
 		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-		consumer.subscribe(Arrays.asList("message_public", "test2"));
+		consumer.subscribe(Arrays.asList("message_public", "test2")); // 可以订阅多个topic
 		
 		final int minBatchSize = 10;
 		try {
@@ -66,6 +66,7 @@ public class KafkaConsumerSample {
 					System.out.println(record.offset() + ": " + record.value() + " offset=" + record.offset());
 			}
 		} finally {
+			consumer.commitSync();
 			consumer.close();
 
 		}
--- src/main/java/kafka/KafkaProducerSample.java
+++ src/main/java/kafka/KafkaProducerSample.java
@@ -9,17 +9,16 @@ public class KafkaProducerSample {
 	public static void main(String[] args) {
 		Properties props = new Properties();
 		props.put("bootstrap.servers", "localhost:9092");// broker的地址
-		props.put("acks", "all");
+		props.put("acks", "all"); // 需要客户端返回回执
 		props.put("retries", Integer.MAX_VALUE);
-		props.put("max.in.flight.requests.per.connection", 1);
+		props.put("max.in.flight.requests.per.connection", 1); // 发送端缓存
 		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		props.put("schema.registry.url", "http://localhost:8081");
-		
-		KafkaProducer kafkaProducer = new KafkaProducer<>(props);
-		
-		// topic，value
-		kafkaProducer.send(new ProducerRecord<>("udemy-reviews", ""));
-		
+		props.put("enable.idempotence", "true"); // 幂等性
+		KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(props);
+		// topic，value（partition和key是可选字段）
+		kafkaProducer.send(new ProducerRecord<String, String>("udemy-reviews", ""));
+		kafkaProducer.close();
 	}
 }
